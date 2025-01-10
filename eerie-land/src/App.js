063a0572a1e6ac0e6558832ec46aea09a133267b
Malika <malikaoubilla@gmail.com>
2025-01-10 16:01:31 +0100

    hello world

---
+++ eerie-land/src/App.js
@@ -0,0 +1,19 @@
+import logo from './assets/logo.png';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        hello
+      </header>
+      hello2
+      <div>
+        <p>hello3</p>
+      </div>
+    </div>
+  );
+}
+
+export default App;
